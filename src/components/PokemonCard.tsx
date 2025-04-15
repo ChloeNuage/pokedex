2c620ea3e671611fc1e92f0e8b73d35ce50a62d4
Chloe <chloe.bretnacher@gmail.com>
2025-04-15 15:01:24 +0200

    partie 5 exo

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,12 +1,22 @@
+interface Pokemon {
+  id: number;
+  name: string;
+  imgSrc?: string; // Le '?' rend imgSrc optionnel
+  // Ajoutez d'autres propriétés si nécessaire
+}
 
-function PokemonCard({pokemon}) {
-  console.log({pokemon});
+interface PokemonCardProps {
+  pokemon: Pokemon;
+}
+
+function PokemonCard({ pokemon }: PokemonCardProps) {
+  console.log({ pokemon });
   return (
     <figure>
-      {pokemon.imgSrc !== undefined ? (
+      {pokemon.imgSrc ? (
         <img src={pokemon.imgSrc} alt={pokemon.name} />
       ) : (
-         <p>???</p>
+        <p>???</p>
       )}
       <figcaption>{pokemon.name}</figcaption>
     </figure>
