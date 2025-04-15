e0ba4830ebb731c21a593676c892cbc3edde1a08
Chloe <chloe.bretnacher@gmail.com>
2025-04-15 15:04:16 +0200

    ecercÇce 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import './App.css'
-
 import PokemonCard from "./components/PokemonCard";
 
-const pokemonList = [
+interface Pokemon {
+  id: number;
+  name: string;
+  imgSrc?: string;
+}
+
+const pokemonList: Pokemon[] = [
   {
+    id: 1, // Ajout de l'ID
     name: "bulbasaur",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    id: 2, // Ajout de l'ID
     name: "mew",
   },
 ];
 
-
 function App() {
   return (
-    <div> <PokemonCard pokemon={pokemonList[0]} /> </div>
+    <div>
+      <PokemonCard pokemon={pokemonList[0]} />
+    </div>
   );
 }
-
-
 
 export default App;
